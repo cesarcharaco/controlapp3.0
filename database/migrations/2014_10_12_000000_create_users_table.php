0cb58eedac14921646211bc645b6d606e8bb20ae
ccharaco <jcesarchg9@gmail.com>
2020-03-22 11:28:38 -0400

    arreglo en tablas

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,11 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('rut');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('tipo_usuario',['Admin','Residente','Otro']);
             $table->rememberToken();
             $table->timestamps();
         });
