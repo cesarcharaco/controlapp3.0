bc9a951cbee5fa7ba783a4fe8c5e90d1dbaab6b3
ccharaco <jcesarchg9@gmail.com>
2020-04-21 15:35:13 -0400

    restaurando iconos

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -23,29 +23,11 @@
 		  });
 
 		$('.carrousel').slick({
-		  	centerMode: true,
-			centerPadding: '60px',
-			slidesToShow: 3,
-			responsive: [
-			{
-			  breakpoint: 768,
-			  settings: {
-			    arrows: false,
-			    centerMode: true,
-			    centerPadding: '40px',
-			    slidesToShow: 3
-			  }
-			},
-			{
-			  breakpoint: 480,
-			  settings: {
-			    arrows: false,
-			    centerMode: true,
-			    centerPadding: '40px',
-			    slidesToShow: 1
-			  }
-			}
-			]
+		  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
 		});
+
 	});
 </script>
