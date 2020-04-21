5908ef99f562df9d41818565110d99c65ed11470
ccharaco <jcesarchg9@gmail.com>
2020-04-20 23:28:24 -0400

    ajustando scripts y css en layouts

--- resources/views/layouts/css.blade.php
+++ resources/views/layouts/css.blade.php
@@ -1,21 +1,22 @@
-<link rel="stylesheet" type="text/css" href="slick/slick.css"/>
-<link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
+
+<link rel="stylesheet" type="text/css" href="{{ asset('slick/slick-theme.css') }}"/>
 <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+{{-- <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}"> --}}
 <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.min.css') }}">
 <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
 <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
-<link href="{{ asset('assets/css/app.css') }}" rel="stylesheet" type="text/css" />
+{{-- <link href="{{ asset('assets/css/app.css') }}" rel="stylesheet" type="text/css" /> --}}
 <link href="{{ asset('assets/css/app.min.css') }}" rel="stylesheet" type="text/css" />
 
 
 <link href="{{ asset('assets/css/select2.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('slick/slick.css') }}" rel="stylesheet" type="text/css" />
 <!-- -->
 <!-- Fonts -->
 <link rel="dns-prefetch" href="//fonts.gstatic.com">
 <!-- Styles -->
-<!-- <link href="{{ asset('toastr/build/toastr.css') }}" rel="stylesheet"/> -->
-<!-- <script src="{{ asset('resources/css/toastr.css') }}"></script> -->
+{{--  <link href="{{ asset('toastr/build/toastr.css') }}" rel="stylesheet"/> --}}
+{{-- <script src="{{ asset('resources/css/toastr.css') }}"></script>  --}}
 
 
 <!-- App css -->
--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -1,14 +1,14 @@
 <!-- <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script> -->
 <!-- <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script> -->
-<script src="{{ asset('assets/js/jquery.js') }}"></script>
+{{-- <script src="{{ asset('assets/js/jquery.js') }}"></script> --}}
 
 
-<!-- <script src="{{ asset('assets/js/app.min.js') }}" ></script> -->
+<script src="{{ asset('assets/js/app.min.js') }}" ></script>
 <script src="{{ asset('assets/js/vendor.min.js') }}"></script>
 <script src="{{ asset('assets/js/select2.min.js') }}"></script>
 
-<script src="{{ asset('js/app.js') }}"></script>
-<script type="text/javascript" src="slick/slick.min.js"></script>
+{{-- <script src="{{ asset('js/app.js') }}"></script> --}}
+<script type="text/javascript" src="{{ asset('slick/slick.min.js') }}"></script>
 <script type="text/javascript">
 	$(function () {
 		
