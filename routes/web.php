d386d76936f2a84f0cec2a61a1c3aafcabe35f7a
ccharaco <jcesarchg9@gmail.com>
2020-04-28 13:48:59 -0400

    arreglando auth

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::group(['middleware' => ['web']], function () {
+Route::group(['middleware' => ['web','auth']], function () {
 	Route::get('/home', 'HomeController@index')->name('home');
 	Route::resource('residentes','ResidentesController');
 
