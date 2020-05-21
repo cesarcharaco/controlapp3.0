2c0313f1e52ecb935efab648f0dbe9e83a48d2da
Jhon-Dante <imagine.breacker@gmail.com>
2020-05-21 19:04:10 -0400

    Vista Reportes Específicos - PDF

--- resources/views/reportes/PDF/ReporteEspecifico.blade.php
+++ resources/views/reportes/PDF/ReporteEspecifico.blade.php
@@ -2,32 +2,132 @@
 <html>
 <head>
 	<title>Reporte Específico</title>
+	<style type="text/css">
+
+		body {
+			/*background-image: url('../../public/assets/images/fondos.png');
+			background-repeat: no-repeat;
+			background-attachment: fixed;*/
+
+		  	font-family: 'helvetica neue', helvetica, arial, sans-serif;
+		}
+
+		table {
+			table-layout: fixed;
+			width: 100%;
+			border-collapse: collapse;
+			border: 3px solid grey;
+  			border-radius: 1em;
+  			overflow: hidden;
+		}
+
+		thead th:nth-child(1) {
+		  width: 30%;
+		}
+
+		thead th:nth-child(2) {
+		  width: 20%;
+		}
+
+		thead th:nth-child(3) {
+		  width: 15%;
+		}
+
+		thead th:nth-child(4) {
+		  width: 35%;
+		}
+
+		th, td {
+		  padding: 20px;
+		}
+
+		/* --------------------------------------------------- */
+
+		thead th, tfoot th {
+		  font-family: 'Rock Salt', cursive;
+		}
+
+		th {
+		  /*letter-spacing: 2px;*/
+		}
+
+		td {
+		  letter-spacing: 1px;
+		}
+
+		tbody td {
+		  text-align: center;
+		}
+
+		tfoot th {
+		  text-align: right;
+		}
+
+
+		/* --------------------------------------------------------- */
+
+		thead, tfoot {
+		  color: white;
+		  text-shadow: 1px 1px 1px black;
+		}
+
+		thead th, tfoot th, tfoot td {
+		  background: linear-gradient(to bottom, rgba(0,0,0,0.1), rgba(0,0,0,0.5));
+		  border: 3px solid blue;
+		}
+
+
+		/* --------------------------------------------------------- */
+		tbody tr:nth-child(odd) {
+		  background-color: white;
+		}
+
+		tbody tr:nth-child(even) {
+		  background-color: #D5D7D8;
+		}
+
+		table {
+		  background-color: #D5D7D8;
+		}
+	</style>
 </head>
 <body>
-	<hr>
+	<div style="float: left">
+		<table style="width: 500px !important;">
+			<tbody>
+				<tr>
+					<th>Año</th>
+				</tr>
+				<tr>
+					<td>{{ $anio }}</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	<div style="text-align: right">
+		<img width="250" height="250" style="border-radius: 50px;" src="../public/assets/images/logo.jpg">
+	</div>
 @if(count($residentes)>0)
 	<table width="100%" border="1">
-		<tr><th colspan="6">RESIDENTES</th></tr>
+		<!-- <tr><th colspan="6">RESIDENTES</th></tr> -->
 		@for($i=0; $i < count($meses); $i++)	
 		<tr>
-			<th><div align="float-left">AÑO: {{ $anio }}</div></th>
-			<th><div align="float-right">MES:{{ meses($meses[$i]) }}</div></th>
-			<th colspan="4"></th>
+			<th colspan="6" align="center"><div align="float-right">MES:{{ meses($meses[$i]) }}</div></th>
 		</tr>
 			@foreach($residentes as $key)
 				<tr>
 				<th>Inmueble(s)</th>
 				<th>Nombre residente</th>
 				<th>Rut/Clave</th>
-				<th>Correo</th>
-				<th colspan="2">Teléfono de contacto</th>
+				<th colspan="2">Correo</th>
+				<th>Teléfono de contacto</th>
 			</tr>
 			<tr>
 				<td>{{ inmuebles_asig($key->id) }}</td>
 				<td>{{ $key->apellidos }}, {{ $key->nombres }}</td>
 				<td>{{ $key->rut }}</td>
-				<td>{{ $key->email }}</td>
-				<td colspan="2">{{ $key->telefono }}</td>
+				<td colspan="2">{{ $key->email }}</td>
+				<td>{{ $key->telefono }}</td>
 			</tr>
 			<tr>
 				<th>Estacionamiento(s)</th>
@@ -50,20 +150,21 @@
 				<td></td>
 			</tr>
 			<tr>
-				<td colspan="6" style="background-color: black;"><div class="page-break"></div></td>
+				<td colspan="6" style="background-color: gray;"><div class="page-break"></div></td>
 			</tr>
 
 			@endforeach
-			<tr>
+			<!-- <tr>
 				<td colspan="6" style="background-color: blue;"><br></td>
-			</tr>
+			</tr> -->
 		@endfor
 		<tr>
-			<td colspan="6" style="background-color: green;"><br></td>
+			<td colspan="6" style="background-color: blue;"><br></td>
 		</tr>
 	</table>
 @endif
 @if(count($inmuebles)>0)
+<hr><br>
 <table width="100%" border="1">
 	<tbody>
 		<tr>
@@ -71,9 +172,7 @@
 		</tr>
 		@for($i=0; $i < count($meses); $i++)
 			<tr>
-				<th>Año: {{ $anio }} </th>
-				<th >Mes: {{ meses($meses[$i]) }}</th>
-				<th colspan="4"></th>
+				<th colspan="6">Mes: {{ meses($meses[$i]) }}</th>
 			</tr>
 		<tr>
 			<th>IDEM</th>
