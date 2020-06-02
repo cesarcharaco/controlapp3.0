9e0b1c81360e04eadb061f025d0f90d3a2f09e2b
Jhon-Dante <imagine.breacker@gmail.com>
2020-06-01 21:14:05 -0400

    Cambio de vista - pagar - residentes

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -597,7 +597,7 @@
 					                    '</div>'+
 					                    '<div class="col-md-4">'+
 					                    	'<div class="form-group">'+
-						                        '<div style="font-size: 2em; ">'+
+						                        '<div style="font-size: 1em; ">'+
 					                                '<svg class="bi bi-lock-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">'+
 													  '<rect width="11" height="9" x="2.5" y="7" rx="2"/>'+
 													  '<path fill-rule="evenodd" d="M4.5 4a3.5 3.5 0 1 1 7 0v3h-1V4a2.5 2.5 0 0 0-5 0v3h-1V4z"/>'+
