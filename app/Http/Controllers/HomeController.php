bbcc39d4df284b644e4324d09e7f837d4928ed01
ccharaco <jcesarchg9@gmail.com>
2020-06-18 11:45:30 -0400

    validando usuario admin activo

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,7 +10,7 @@ use App\PagosComunes;
 use App\Estacionamientos;
 use App\Inmuebles;
 use App\Anuncios;
-
+use App\UsersAdmin;
 class HomeController extends Controller
 {
 
@@ -35,21 +35,37 @@ class HomeController extends Controller
     public function index()
     {
         //toastr()->warning('Éxito!', 'Usuario Admin registrado');
+        $entrar="";
         $id_admin=id_admin(\Auth::user()->email);
-        $anio=Date('Y');
-        $noticias=Noticias::where('id_admin',$id_admin)->get();
-        $notificaciones=Notificaciones::where('id_admin',$id_admin)->get();
-        $residentes=Residentes::where('id_admin',$id_admin)->get();
-        $residente=Residentes::where('id_usuario',\Auth::user()->id)->first();
-        $estacionamientos=Estacionamientos::where('status','<>','Ocupado')->where('id_admin',$id_admin)->get();
-        $inmuebles=Inmuebles::where('status','<>','No Disponible')->where('id_admin',$id_admin)->get();
+        if(\Auth::user()->tipo_usuario=="Admin"){
+            $buscar=UsersAdmin::find($id_admin);
+            if ($buscar->status=="activo") {
+                $entrar="Si";
+            }
+        }
+        if (($entrar=="Si" && \Auth::user()->tipo_usuario=="Admin") || \Auth::user()->tipo_usuario!="Admin") {
+            $anio=Date('Y');
+            $noticias=Noticias::where('id_admin',$id_admin)->get();
+            $notificaciones=Notificaciones::where('id_admin',$id_admin)->get();
+            $residentes=Residentes::where('id_admin',$id_admin)->get();
+            $residente=Residentes::where('id_usuario',\Auth::user()->id)->first();
+            $estacionamientos=Estacionamientos::where('status','<>','Ocupado')->where('id_admin',$id_admin)->get();
+            $inmuebles=Inmuebles::where('status','<>','No Disponible')->where('id_admin',$id_admin)->get();
 
-        $buscarPInmuebles= PagosComunes::where('tipo','Inmueble')->where('anio',$anio)->where('id_admin',$id_admin)->get();
-        $buscarPEstaciona= PagosComunes::where('tipo','Estacionamiento')->where('anio',$anio)->where('id_admin',$id_admin)->get();
-        $anuncios=Anuncios::all();
+            $buscarPInmuebles= PagosComunes::where('tipo','Inmueble')->where('anio',$anio)->where('id_admin',$id_admin)->get();
+            $buscarPEstaciona= PagosComunes::where('tipo','Estacionamiento')->where('anio',$anio)->where('id_admin',$id_admin)->get();
+            $anuncios=Anuncios::all();
 
-        //dd('-------------');
+            //dd('-------------');
 
-        return view('home', compact('noticias', 'notificaciones','residentes','residente','buscarPInmuebles','buscarPEstaciona','anuncios'));
+            return view('home', compact('noticias', 'notificaciones','residentes','residente','buscarPInmuebles','buscarPEstaciona','anuncios'));    
+        } else {
+            # code...
+            
+            flash('Usuario Suspendido','warning')->important();
+            return redirect()->to('/');
+        }
+        
+        
     }
 }
