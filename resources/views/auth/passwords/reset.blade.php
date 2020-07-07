e45c5ba5002bad921f7717f0820b62b57ab80486
Jhon-Dante <imagine.breacker@gmail.com>
2020-07-06 23:28:02 -0400

    Mensajes reestablecer contraseña

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -47,26 +47,35 @@
                             @enderror
 
                             <input type="hidden" name="token" value="{{ $token }}">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="email">{{ __('E-Mail Address') }}</label>
 
-                            <div class="form-group">
-                                <label for="email">{{ __('E-Mail Address') }}</label>
+                                            <input id="email" type="email" disabled class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
 
-                                <input id="email" type="email" disabled class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="password">{{ __('Password') }}</label>
 
-                            </div>
+                                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
-                            <div class="form-group">
-                                <label for="password">{{ __('Password') }}</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group ">
+                                            <label for="password-confirm">{{ __('Confirm Password') }}</label>
 
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                            </div>
-
-                            <div class="form-group ">
-                                <label for="password-confirm">{{ __('Confirm Password') }}</label>
-
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
+                                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                        </div>
+                                    </div>
+                                </div>
 
                             <div class="row">
                                 <div class="col-md-12">
